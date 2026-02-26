b9a209071f039014abc1bc9d1541ecf7e0621db5
Daff444 <dafne.coutinho@icloud.com>
2026-02-26 20:17:41 -0300

    páginas da Dafne

---
+++ app/adimini_prest/page.js
@@ -0,0 +1,10 @@
+export default function AdiminiPrest(){
+    return(
+        <div>
+            <h1>Painel Administrativo Prestador</h1>
+        </div>
+    )
+    
+}
+
+
---
+++ app/cad_prest/page.js
@@ -0,0 +1,8 @@
+export default function CadPrest(){
+    return(
+        <div>
+            <h1>Cadastro de prestador</h1>
+        </div>
+    )
+}
+
